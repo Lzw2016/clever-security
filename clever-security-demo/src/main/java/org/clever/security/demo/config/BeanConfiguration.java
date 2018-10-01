23676294d34fe82c549cebb44c7c10485cbafb63
Lzw2016 <lizhiwei1993>
2018-10-01 16:40:52 +0800

    优化

---
+++ clever-security-demo/src/main/java/org/clever/security/demo/config/BeanConfiguration.java
@@ -0,0 +1,14 @@
+package org.clever.security.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 作者： lzw<br/>
+ * 创建时间：2018-10-01 16:30 <br/>
+ */
+@Configuration
+@Slf4j
+public class BeanConfiguration {
+
+}
