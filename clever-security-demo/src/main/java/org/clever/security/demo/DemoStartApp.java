027f10b575fd32a1713d003d0967165719b92cbc
李志伟 <lizw@jzteyao.com>
2018-10-02 23:50:58 +0800

    设置 redisNamespace

--- clever-security-demo/src/main/java/org/clever/security/demo/DemoStartApp.java
+++ clever-security-demo/src/main/java/org/clever/security/demo/DemoStartApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
+import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import java.util.TimeZone;
@@ -17,7 +18,12 @@ import java.util.TimeZone;
 @Slf4j
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"org.clever.security.client"})
-@EnableRedisHttpSession(cleanupCron = "0/3 * * * * *", maxInactiveIntervalInSeconds = 60)
+@EnableRedisHttpSession(
+        cleanupCron = "0/3 * * * * *",
+        maxInactiveIntervalInSeconds = 60,
+        redisNamespace = "spring:session:demo",
+        redisFlushMode = RedisFlushMode.ON_SAVE
+)
 @SpringBootApplication(scanBasePackages = {"org.clever"})
 public class DemoStartApp {
     public static void main(String[] args) {
