60698ea6c001ddb2faba4509dc0b7e07ac961057
lizw <lzw1000000@163.com>
2019-04-29 10:32:53 +0800

    基本改造完成，未调试 RefreshTokenController

--- clever-security-embed/src/main/java/org/clever/security/controller/RefreshTokenController.java
+++ clever-security-embed/src/main/java/org/clever/security/controller/RefreshTokenController.java
@@ -3,41 +3,33 @@ package org.clever.security.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.clever.security.Constant;
 import org.clever.security.dto.request.RefreshTokenReq;
 import org.clever.security.dto.response.JwtLoginRes;
-import org.clever.security.dto.response.UserRes;
 import org.clever.security.repository.RedisJwtRepository;
 import org.clever.security.token.JwtAccessToken;
 import org.clever.security.token.JwtRefreshToken;
 import org.clever.security.utils.AuthenticationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 作者： lzw<br/>
- * 创建时间：2018-11-12 17:03 <br/>
+ * 创建时间：2019-04-29 10:29 <br/>
  */
-@Api("当前登录用户信息")
+@Api("JwtToken刷新登录令牌")
+@ConditionalOnProperty(prefix = Constant.ConfigPrefix, name = "loginModel", havingValue = "jwt")
 @RestController
 @Slf4j
-public class LoginUserController {
+public class RefreshTokenController {
 
     @Autowired
     private RedisJwtRepository redisJwtRepository;
-
-    @ApiOperation("获取当前登录用户信息")
-    @GetMapping("/login/user_info.json")
-    public UserRes currentLoginUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return AuthenticationUtils.getUserRes(authentication);
-    }
 
     @ApiOperation("使用刷新令牌延长登录JwtToken过期时间")
     @PutMapping("/login/refresh_token.json")
