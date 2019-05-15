db832f7972b01006c510b1d122110fc4e1efc5de
Lzw2016 <lizhiwei1993>
2019-05-15 22:54:52 +0800

    访问 clever-security-server 服务API的授权Token请求头

--- clever-security-client/src/main/java/org/clever/security/config/CleverSecurityFeignConfiguration.java
+++ clever-security-client/src/main/java/org/clever/security/config/CleverSecurityFeignConfiguration.java
@@ -1,12 +1,35 @@
 package org.clever.security.config;
 
-import org.springframework.context.annotation.Configuration;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.clever.common.utils.spring.SpringContextHolder;
+
+import java.util.Map;
 
 /**
  * 作者： lzw<br/>
  * 创建时间：2019-05-15 17:58 <br/>
  */
-@Configuration
-public class CleverSecurityFeignConfiguration {
-    
+@Slf4j
+public class CleverSecurityFeignConfiguration implements RequestInterceptor {
+
+    /**
+     * 服务访问Token
+     */
+    private CleverSecurityAccessTokenConfig accessTokenConfig;
+
+    @Override
+    public void apply(RequestTemplate template) {
+        if (accessTokenConfig == null) {
+            accessTokenConfig = SpringContextHolder.getBean(CleverSecurityAccessTokenConfig.class);
+            log.info("读取访问clever-security-server服务API的授权Token请求头: {}", accessTokenConfig.getAccessTokenHeads());
+        }
+        if (accessTokenConfig.getAccessTokenHeads() != null && accessTokenConfig.getAccessTokenHeads().size() > 0) {
+            log.debug("[访问clever-security-server的AccessToken请求头] --> {}", accessTokenConfig.getAccessTokenHeads());
+            for (Map.Entry<String, String> entry : accessTokenConfig.getAccessTokenHeads().entrySet()) {
+                template.header(entry.getKey(), entry.getValue());
+            }
+        }
+    }
 }
