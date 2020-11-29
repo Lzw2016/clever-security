edaff07af30df17062c8b1e752ba14cb403793c9
lizw <1183409807@qq.com>
2020-11-29 16:34:34 +0800

    3.0.0 版本 6%

---
+++ clever-security-embed/src/main/java/org/clever/security/embed/handler/LogoutFailureHandler.java
@@ -0,0 +1,24 @@
+package org.clever.security.embed.handler;
+
+import org.clever.security.embed.event.LogoutFailureEvent;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2020/11/29 16:30 <br/>
+ */
+public interface LogoutFailureHandler {
+
+    /**
+     * 登出失败处理逻辑
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param event    登出失败事件
+     */
+    void onLogoutFailure(HttpServletRequest request, HttpServletResponse response, LogoutFailureEvent event) throws IOException, ServletException;
+}
---
+++ clever-security-embed/src/main/java/org/clever/security/embed/handler/LogoutSuccessHandler.java
@@ -0,0 +1,24 @@
+package org.clever.security.embed.handler;
+
+import org.clever.security.embed.event.LogoutSuccessEvent;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2020/11/29 16:32 <br/>
+ */
+public interface LogoutSuccessHandler {
+
+    /**
+     * 登出成功处理逻辑
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param event    登出成功事件
+     */
+    void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, LogoutSuccessEvent event) throws IOException, ServletException;
+}
