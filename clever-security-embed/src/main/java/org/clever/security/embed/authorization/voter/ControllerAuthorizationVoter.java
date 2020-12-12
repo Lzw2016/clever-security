98c6285b782fe7185040bb945c797f7c21bc6793
lizw <1183409807@qq.com>
2020-12-12 21:48:10 +0800

    3.0.0 版本 53%

--- clever-security-embed/src/main/java/org/clever/security/embed/authorization/voter/ControllerAuthorizationVoter.java
+++ clever-security-embed/src/main/java/org/clever/security/embed/authorization/voter/ControllerAuthorizationVoter.java
@@ -1,7 +1,10 @@
 package org.clever.security.embed.authorization.voter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.clever.security.embed.config.SecurityConfig;
 import org.clever.security.embed.exception.AuthorizationInnerException;
+import org.clever.security.entity.EnumConstant;
+import org.clever.security.entity.Permission;
 import org.clever.security.model.SecurityContext;
 import org.springframework.core.Ordered;
 import org.springframework.util.Assert;
@@ -10,14 +13,18 @@ import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
+ * Spring MVC Controller访问权限投票
+ * <p>
  * 作者：lizw <br/>
  * 创建时间：2020/12/06 20:27 <br/>
  */
+@Slf4j
 public class ControllerAuthorizationVoter implements AuthorizationVoter {
     /**
-     *
+     * Spring内置的根据request获取Controller Method的工具对象
      */
     private final RequestMappingHandlerMapping requestMappingHandlerMapping;
 
@@ -62,8 +69,21 @@ public class ControllerAuthorizationVoter implements AuthorizationVoter {
             }
             methodParams.append(clazz.getName());
         }
-        // TODO authorization
-
+        // TODO 根据targetClass、targetMethod、methodParams得到权限元数据
+        Permission permission = null;
+        if (permission == null) {
+            // log.info("### 授权通过(当前资源不需要访问权限) [{}#{}] [{}] -> [{}]", targetClass, targetMethod, webPermissionModel.getResourcesUrl(), filterInvocation.getRequestUrl());
+            return VoterResult.PASS;
+        }
+        if (Objects.equals(permission.getEnableAuth(), EnumConstant.Permission_EnableAuth_0)) {
+            // log.info("### 授权通过(当前资源不需要访问权限) [{}#{}] [{}] -> [{}]", targetClass, targetMethod, webPermissionModel.getResourcesUrl(), filterInvocation.getRequestUrl());
+            return VoterResult.PASS;
+        }
+        if (!securityContext.hasPermissions(permission.getStrFlag())) {
+            // log.info("### 授权失败(未授权) [{}#{}] [{}] -> [{}]", targetClass, targetMethod, webPermissionModel.getResourcesUrl(), filterInvocation.getRequestUrl());
+            return VoterResult.REJECT;
+        }
+        // log.info("### 授权通过(已授权) [{}#{}] [{}] -> [{}]", targetClass, targetMethod, webPermissionModel.getResourcesUrl(), filterInvocation.getRequestUrl());
         return VoterResult.PASS;
     }
 }
