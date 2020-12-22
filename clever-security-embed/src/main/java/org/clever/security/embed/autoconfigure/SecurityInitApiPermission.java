5302dd0a6ca11816cc4ab86f2f963a347a8e6f36
lizw <1183409807@qq.com>
2020-12-22 21:05:53 +0800

    3.0.0 版本 80%

--- clever-security-embed/src/main/java/org/clever/security/embed/autoconfigure/SecurityInitApiPermission.java
+++ clever-security-embed/src/main/java/org/clever/security/embed/autoconfigure/SecurityInitApiPermission.java
@@ -1,11 +1,17 @@
 package org.clever.security.embed.autoconfigure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.clever.common.utils.mapper.JacksonMapper;
 import org.clever.security.embed.config.SecurityConfig;
+import org.clever.security.jackson2.CleverSecurityJackson2Module;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+
+import java.util.List;
 
 /**
  * 作者：lizw <br/>
@@ -20,10 +26,12 @@ public class SecurityInitApiPermission implements CommandLineRunner {
      * 全局配置
      */
     private final SecurityConfig securityConfig;
+    private final List<ObjectMapper> objectMapperList;
     protected boolean initialized = false;
 
-    public SecurityInitApiPermission(SecurityConfig securityConfig) {
+    public SecurityInitApiPermission(SecurityConfig securityConfig, ObjectProvider<List<ObjectMapper>> objectMapperList) {
         this.securityConfig = securityConfig;
+        this.objectMapperList = objectMapperList.getIfAvailable();
     }
 
     @Override
@@ -32,5 +40,11 @@ public class SecurityInitApiPermission implements CommandLineRunner {
             return;
         }
         initialized = true;
+        JacksonMapper.getInstance().getMapper().registerModule(CleverSecurityJackson2Module.instance);
+        if (objectMapperList != null) {
+            for (ObjectMapper objectMapper : objectMapperList) {
+                objectMapper.registerModule(CleverSecurityJackson2Module.instance);
+            }
+        }
     }
 }
--- clever-security-model/src/main/java/org/clever/security/jackson2/CleverSecurityJackson2Module.java
+++ clever-security-model/src/main/java/org/clever/security/jackson2/CleverSecurityJackson2Module.java
@@ -9,6 +9,8 @@ import org.clever.security.model.SecurityContext;
  * 创建时间：2020/12/19 13:44 <br/>
  */
 public class CleverSecurityJackson2Module extends SimpleModule {
+    public static final CleverSecurityJackson2Module instance = new CleverSecurityJackson2Module();
+
     public CleverSecurityJackson2Module() {
         super(CleverSecurityJackson2Module.class.getName(), new Version(1, 0, 0, null, null, null));
     }
