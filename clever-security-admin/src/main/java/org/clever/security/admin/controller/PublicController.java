e10674331dde75b581d275cfe4b07f307772ab5e
lizw <1183409807@qq.com>
2020-12-29 21:17:09 +0800

    3.0.0 版本 86% - bug fix

--- clever-security-admin/src/main/java/org/clever/security/admin/controller/PublicController.java
+++ clever-security-admin/src/main/java/org/clever/security/admin/controller/PublicController.java
@@ -1,6 +1,7 @@
 package org.clever.security.admin.controller;
 
 import org.clever.common.model.response.AjaxMessage;
+import org.clever.security.annotation.UrlAuthorization;
 import org.clever.security.entity.Domain;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,7 @@ public class PublicController {
         return new AjaxMessage<>(domain, "成功");
     }
 
+    @UrlAuthorization(title = "测试02", description = "测试接口", strFlag = "PublicController_t02_#1")
     @GetMapping("/t02/{param}")
     public Object t02(@PathVariable("param") String param) {
         return new AjaxMessage<>(param, "成功");
