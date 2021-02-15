8acaa78780d17c6b29c8f65aed4c7cbe3db5e7e3
lizw <1183409807@qq.com>
2021-02-15 22:59:19 +0800

    3.0.0 版本 95%

---
+++ clever-security-server/src/main/java/org/clever/security/controller/admin/DomainController.java
@@ -0,0 +1,14 @@
+package org.clever.security.controller.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2021/02/15 22:58 <br/>
+ */
+@RestController
+@RequestMapping("/security/admin/api")
+public class DomainController {
+
+}
