c54e3c229e5c6e1c4e50b55cad1911b2a2e266b9
lizw <1183409807@qq.com>
2021-02-18 20:07:24 +0800

    3.0.0 版本 95%

--- clever-security-server/src/main/java/org/clever/security/controller/admin/UserSecurityContextController.java
+++ clever-security-server/src/main/java/org/clever/security/controller/admin/UserSecurityContextController.java
@@ -3,11 +3,10 @@ package org.clever.security.controller.admin;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.clever.security.dto.request.admin.UserSecurityContextQueryReq;
 import org.clever.security.dto.response.admin.UserSecurityContextQueryRes;
+import org.clever.security.entity.UserSecurityContext;
 import org.clever.security.service.admin.UserSecurityContextService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 作者：lizw <br/>
@@ -24,4 +23,8 @@ public class UserSecurityContextController {
         return userSecurityContextService.pageQuery(req);
     }
 
+    @PostMapping("/user_security_context/reload")
+    public UserSecurityContext reloadUserSecurityContext(@RequestParam("id") Long id) {
+        return userSecurityContextService.reloadUserSecurityContext(id);
+    }
 }
--- clever-security-server/src/main/java/org/clever/security/service/admin/UserSecurityContextService.java
+++ clever-security-server/src/main/java/org/clever/security/service/admin/UserSecurityContextService.java
@@ -1,10 +1,14 @@
 package org.clever.security.service.admin;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.clever.common.exception.BusinessException;
+import org.clever.security.dto.request.LoadContextReq;
 import org.clever.security.dto.request.admin.DomainQueryReq;
 import org.clever.security.dto.request.admin.UserSecurityContextQueryReq;
 import org.clever.security.dto.response.admin.UserSecurityContextQueryRes;
+import org.clever.security.entity.UserSecurityContext;
 import org.clever.security.mapper.UserSecurityContextMapper;
+import org.clever.security.service.AuthSupportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +22,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserSecurityContextService {
     @Autowired
     private UserSecurityContextMapper userSecurityContextMapper;
+    @Autowired
+    private AuthSupportService authSupportService;
 
     public IPage<UserSecurityContextQueryRes> pageQuery(UserSecurityContextQueryReq req) {
         req.addOrderFieldMapping("id", "a.id");
@@ -33,5 +39,18 @@ public class UserSecurityContextService {
             req.addOrderField("createAt", DomainQueryReq.DESC);
         }
         return req.result(userSecurityContextMapper.pageQuery(req));
+    }
+
+    @Transactional
+    public UserSecurityContext reloadUserSecurityContext(Long id) {
+        UserSecurityContext userSecurityContext = userSecurityContextMapper.selectById(id);
+        if (userSecurityContext == null) {
+            throw new BusinessException("数据不存在");
+        }
+        userSecurityContextMapper.deleteById(id);
+        LoadContextReq loadContextReq = new LoadContextReq(userSecurityContext.getDomainId());
+        loadContextReq.setUid(userSecurityContext.getUid());
+        authSupportService.loadContext(loadContextReq);
+        return userSecurityContextMapper.getByUid(userSecurityContext.getDomainId(), userSecurityContext.getUid());
     }
 }
